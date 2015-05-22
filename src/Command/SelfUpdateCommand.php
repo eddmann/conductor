b1c5a17300bf50244cabe1b0d2bf8f1d43b646dc
Edd Mann <the@eddmann.com>
2015-05-22 13:36:25 +0100

    Add box support

---
+++ src/Command/SelfUpdateCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MyBuilder\Conductor\Command;
+
+use Herrera\Phar\Update\Manager;
+use Herrera\Phar\Update\Manifest;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SelfUpdateCommand extends Command
+{
+    const MANIFEST_FILE = 'http://eddmann.github.io/conductor/manifest.json';
+
+    protected function configure()
+    {
+        $this
+            ->setName('self-update')
+            ->setAliases(array('selfupdate'))
+            ->setDescription('Updates to the latest version of Conductor');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $manager = new Manager(Manifest::loadFile(self::MANIFEST_FILE));
+        $manager->update($this->getApplication()->getVersion(), true);
+    }
+}
